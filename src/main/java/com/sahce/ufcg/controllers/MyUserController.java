df8b2749d1d14b2a7529c0427ab676e601e15099
Italo Modesto Pereira <italo.pereira@ccc.ufcg.edu.br>
2022-11-08 00:23:19 -0300

    getUser endpoint enabled and removed authorities from login response

--- src/main/java/com/sahce/ufcg/controllers/MyUserController.java
+++ src/main/java/com/sahce/ufcg/controllers/MyUserController.java
@@ -2,12 +2,11 @@ package com.sahce.ufcg.controllers;
 
 import com.sahce.ufcg.dtos.myUser.MyUserDtoResponse;
 import com.sahce.ufcg.dtos.myUser.MyUserDtoRequest;
-import com.sahce.ufcg.models.MyUser;
 import com.sahce.ufcg.services.MyUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.GrantedAuthority;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,8 +30,8 @@ public class MyUserController {
         return new ResponseEntity<>(service.activeUser(user), HttpStatus.OK);
     }
 
-//    @GetMapping("/protected/users/{email}")
-//    public ResponseEntity<MyUserDtoResponse> getAuthoritiesByUser(@PathVariable String email){
-//        return new ResponseEntity<>(service.getUserByEmail(email), HttpStatus.OK);
-//    }
+    @GetMapping("/protected/users/{email}")
+    public ResponseEntity<MyUserDtoResponse> getAuthoritiesByUser(@PathVariable String email){
+        return new ResponseEntity<>(service.getUserByEmail(email), HttpStatus.OK);
+    }
 }
